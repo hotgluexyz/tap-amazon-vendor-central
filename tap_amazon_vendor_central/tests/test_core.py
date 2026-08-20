37e78b3b867abadfd02b13c599318bb49e36b3cf
César Lima <cesar@hotglue.io>
2026-08-20 15:55:11 -0300

    HGI-11006: add repo cleanup with README, tox, ruff, GHA, and Jenkins

--- tap_amazon_vendor_central/tests/test_core.py
+++ tap_amazon_vendor_central/tests/test_core.py
@@ -14,13 +14,9 @@ SAMPLE_CONFIG = {
 }
 
 
-# Run standard built-in tap tests from the SDK:
 def test_standard_tap_tests():
     """Run standard tap tests from the SDK."""
     tests = get_standard_tap_tests(TapAmazonVendorCentral, config=SAMPLE_CONFIG)
     # skip connection/sync tests
     for test in tests[:2]:
         test()
-
-
-# TODO: Create additional tests as appropriate for your tap.
